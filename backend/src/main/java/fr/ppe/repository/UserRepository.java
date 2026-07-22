466cd1904260528900b080171767303824fdbc61
st807060-afk <st807060@gmail.com>
2026-07-22 12:27:27 +0200

    Create UserRepository.java

---
+++ backend/src/main/java/fr/ppe/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package fr.ppe.repository;
+
+import fr.ppe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+}
