196f5f9d8ac9536506ff7879a5f60ba7559644b6
st807060-afk <st807060@gmail.com>
2026-07-22 13:48:01 +0200

    Create SupplierRepository.java

---
+++ backend/src/main/java/fr/ppe/repository/SupplierRepository.java
@@ -0,0 +1,12 @@
+package fr.ppe.repository;
+
+import fr.ppe.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+    Optional<Supplier> findByEmail(String email);
+
+}
