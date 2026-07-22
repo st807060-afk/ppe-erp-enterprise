1de3b5ef99598061bffa15619802d5afd1b5b3b3
st807060-afk <st807060@gmail.com>
2026-07-22 12:11:42 +0200

    Create PPEApplication.java

---
+++ backend/src/main/java/fr/ppe/PPEApplication.java
@@ -0,0 +1,13 @@
+package fr.ppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PPEApplication.class, args);
+    }
+
+}
