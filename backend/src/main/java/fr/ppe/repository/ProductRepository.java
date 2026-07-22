980b156de9051933e2b68982fe6fa270fbf720fb
st807060-afk <st807060@gmail.com>
2026-07-22 13:32:09 +0200

    Create ProductRepository.java

---
+++ backend/src/main/java/fr/ppe/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package fr.ppe.repository;
+
+import fr.ppe.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findBySku(String sku);
+
+    Optional<Product> findByBarcode(String barcode);
+
+}
