65edce9003b379d0687312ddfcff7492bdd02d44
st807060-afk <st807060@gmail.com>
2026-07-22 13:40:29 +0200

    Create CustomerRepository.java

---
+++ backend/src/main/java/fr/ppe/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package fr.ppe.repository;
+
+import fr.ppe.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
