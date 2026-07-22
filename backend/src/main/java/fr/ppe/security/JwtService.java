58ad5c26740d7447085da6dbb720bd59a82e3b60
st807060-afk <st807060@gmail.com>
2026-07-22 20:14:35 +0200

    Update JwtService.java

--- backend/src/main/java/fr/ppe/security/JwtService.java
+++ backend/src/main/java/fr/ppe/security/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -52,5 +53,10 @@ public class JwtService {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && validateToken(token);
     }
 }
